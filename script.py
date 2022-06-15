8ccb34022dca6846e80ef38a416e7b39e0ffd7a6
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-06-15 18:16:54 +0530

    assign 2 sequential

--- script.py
+++ script.py
@@ -4,7 +4,15 @@ N = 100000
 
 f = open("generate.txt", "w")
 
+f.write(str(N)+"\n")
 for i in range(N):
-    v = str(random.randint(-999999, 999999))
+    v = str(random.uniform(-2, 2))
+    v = v[0:5]
     f.write(v+"\n")
+
+for i in range(N):
+    v = str(random.uniform(-2, 1))
+    v = v[0:5]
+    f.write(v+"\n")
+
 f.close()
