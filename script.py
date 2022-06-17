c9c0f11f20a1011e7d25a1a8ca72b7abb87d4caa
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-06-17 09:15:19 +0530

    testcase for assign 4

--- script.py
+++ script.py
@@ -1,28 +1,44 @@
+from enum import unique
 import random
-
-N = 100
 
 f = open("generate.txt", "w")
 
-K = 3
-L = 512
-M = 512
+rows = 500
+cols = 500
+nonZeroNumbers = 3000
 
-for i in range(K):
-    for j in range(L):
-        for k in range(M):
-            v = str(random.randint(-1, 2))
-            f.write(v+" ")
-        f.write("\n")
+# 3000 Unique pairs for integers between 0 - 999
 
-A = 3
-B = 3
-C = 3
-for i in range(A):
-    for j in range(B):
-        for k in range(C):
-            v = str(random.randint(-1, 2))
-            f.write(v+" ")
-        f.write("\n")
+lstOfPairs = []
+while(len(lstOfPairs)<=nonZeroNumbers-1):
+    uniquePair = [random.randint(0, rows-1), random.randint(0, cols-1)]
+    if uniquePair in lstOfPairs:
+        pass
+    lstOfPairs.append(uniquePair)
+
+for l in lstOfPairs:
+    v = str(random.uniform(-2, 2))
+    f.write(str(l[0])+" "+str(l[1])+" "+v+"\n")
+
+for i in range(cols):
+    v = str(random.uniform(-2, 2))
+    f.write(v+"\n")
+
+# for i in range(rows):
+#     for j in range(cols):
+#         for k in range(M):
+#             v = str(random.randint(-1, 2))
+#             f.write(v+" ")
+#         f.write("\n")
+
+# A = 3
+# B = 3
+# C = 3
+# for i in range(A):
+#     for j in range(B):
+#         for k in range(C):
+#             v = str(random.randint(-1, 2))
+#             f.write(v+" ")
+#         f.write("\n")
 
 f.close()
